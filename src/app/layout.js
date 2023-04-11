8d06e6281fb8f8118aebd41e10c941073b2b231f
ias1501 <103571149+ias1501@users.noreply.github.com>
2023-04-11 15:33:57 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,10 @@
+
 import { AuthProvider } from 'src/components/AuthProvider';
 import createClient from 'src/lib/supabase-server';
-
+import Navbar from './Navbar/Navbar';
+import "../app/layout.css";
 import 'src/styles/globals.css';
+import Footer from './Footer/footer';
 
 // do not cache this layout
 export const revalidate = 0;
@@ -18,20 +21,37 @@ export default async function RootLayout({ children }) {
 
   return (
     <html lang="en">
-      <body>
+       
+      <body  >
+       <div>
+        <Navbar/>
+       </div>
+       <section>
+  <div class='air air1'></div>
+  <div class='air air2'></div>
+  <div class='air air3'></div>
+  <div class='air air4'></div>
 
-
-
-        
         <div className="flex min-h-screen flex-col items-center justify-center py-2">
           <main className="flex w-full flex-1 shrink-0 flex-col items-center justify-center px-8 text-center sm:px-20">
             <h1 className="mb-12 text-5xl font-bold sm:text-6xl">
               Wander <span className="font-black text-blue-400">Float</span>
             </h1>
             <AuthProvider accessToken={accessToken}>{children}</AuthProvider>
+
           </main>
+
         </div>
+        </section>
+         
+        
       </body>
+      <footer>
+          <div>
+            <Footer/>
+          </div>
+         </footer> 
+      
     </html>
   );
 }
