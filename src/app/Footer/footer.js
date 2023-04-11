9e1ff31bb958578088c5738d795880979c1c6bac
ias1501 <103571149+ias1501@users.noreply.github.com>
2023-04-11 16:28:12 +0530

    Update footer.js

--- src/app/Footer/footer.js
+++ src/app/Footer/footer.js
@@ -1,5 +1,5 @@
 
-import "../Footer/Footer.css";
+
 
 export default function Footer()
 {
@@ -7,12 +7,7 @@ export default function Footer()
 
 
 <footer class="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
-<div class="waves">
-      <div class="wave" id="wave1"></div>
-      <div class="wave" id="wave2"></div>
-      <div class="wave" id="wave3"></div>
-      <div class="wave" id="wave4"></div>
-    
+
     <div class="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div class="sm:flex sm:items-center sm:justify-between">
             <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
@@ -37,7 +32,7 @@ export default function Footer()
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://flowbite.com/" class="hover:underline">Flowbite™</a>. All Rights Reserved.</span>
-    </div>
+  
 </footer>
 );
 }
